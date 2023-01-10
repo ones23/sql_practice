8e12ba05fa9337a5e5a3bc52244ddc97a1625be3
Anastasia Selyanina <selyanina_01@mail.ru>
2023-01-10 14:34:02 +0500

    learning

---
+++ 2.3.sql
@@ -0,0 +1,13 @@
+INSERT INTO book(title, author, price, amount) 
+VALUES ('Мастер и Маргарита','Булгаков М.А.',670.99,3);
+SELECT * FROM book;
+
+#соединение двух таблиц
+
+INSERT INTO book(title, author, price, amount) 
+VALUES ('Белая гвардия','Булгаков М.А.',540.50,5);
+INSERT INTO book(title, author, price, amount) 
+VALUES ('Идиот','Достоевский Ф.М.',460.00,10);
+INSERT INTO book(title, author, price, amount) 
+VALUES ('Братья Карамазовы','Достоевский Ф.М.',799.01,2);
+SELECT * FROM book;
